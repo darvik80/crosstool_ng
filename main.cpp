141fa29200a86a0bca9d579f82bae4270023e1d0
Ivan Kishchenko <ivan.kishchenko@lazada.com>
2023-02-24 19:05:57 +0700

    image containers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <fmt/format.h>
+#include <boost/system.hpp>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    fmt::format("Hello: {}", 10);
+
     return 0;
 }
